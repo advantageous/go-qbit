1b71a50f3280d97f645f380cf99f2473a862ac71
RichardHightower <richard.m.hightower@gmail.com>
2016-12-15 12:03:35 -0800

    perf improvements. Reduced GC by 1000x

--- qbit/bench-mark_queue_test.go
+++ qbit/bench-mark_queue_test.go
@@ -1,14 +1,19 @@
 package qbit
 
 import (
+	"fmt"
 	"sync/atomic"
 	"testing"
 	"time"
+	//	"runtime"
+	"runtime"
+	"strconv"
 )
 
 func BenchmarkQueue(b *testing.B) {
 
-	const total = 1E9
+	const total = 1E8
+	const numRuns = 10
 
 	counter := int32(0)
 
@@ -29,23 +34,74 @@ func BenchmarkQueue(b *testing.B) {
 
 	b.ResetTimer()
 
-	sendQueue := queueManager.Queue().SendQueue()
+	start := time.Now().Unix()
+	for run := 0; run < numRuns; run++ {
 
-	go func() {
-		for i := 0; i < total; i++ {
-			sendQueue.Send(testItem)
+		startLoop := time.Now().Unix()
+
+		go func() {
+			sendQueue := queueManager.Queue().SendQueue()
+			for i := 0; i < total; i++ {
+				sendQueue.Send(testItem)
+			}
+			sendQueue.FlushSends()
+		}()
+
+		for {
+			<-time.NewTimer(10 * time.Millisecond).C
+			count := atomic.LoadInt32(&counter)
+			if count >= total {
+				break
+			}
 		}
-		sendQueue.FlushSends()
-		<-time.NewTimer(100 * time.Millisecond).C
 
-	}()
+		counter = int32(0)
+		fmt.Printf("Run %d\tElaspsed     %d seconds\n", run, time.Now().Unix()-startLoop)
+		fmt.Printf("      \tTotal Time   %d seconds\n", time.Now().Unix()-start)
 
-	for {
-		<-time.NewTimer(10 * time.Millisecond).C
-		count := atomic.LoadInt32(&counter)
-		if count >= total {
-			break
-		}
+		memStat := &runtime.MemStats{}
+		runtime.ReadMemStats(memStat)
+
+		fmt.Printf("heap %s \t GC count %s\n", uint64ToString(memStat.HeapAlloc), uint32ToString(memStat.NumGC))
+		counter = int32(0)
 	}
 
+}
+
+func uint64ToString(n uint64) string {
+	in := strconv.FormatUint(n, 10)
+	out := make([]byte, len(in)+(len(in)-2+int(in[0]/'0'))/3)
+	if in[0] == '-' {
+		in, out[0] = in[1:], '-'
+	}
+
+	for i, j, k := len(in)-1, len(out)-1, 0; ; i, j = i-1, j-1 {
+		out[j] = in[i]
+		if i == 0 {
+			return string(out)
+		}
+		if k++; k == 3 {
+			j, k = j-1, 0
+			out[j] = ','
+		}
+	}
+}
+
+func uint32ToString(n uint32) string {
+	in := strconv.FormatUint(uint64(n), 10)
+	out := make([]byte, len(in)+(len(in)-2+int(in[0]/'0'))/3)
+	if in[0] == '-' {
+		in, out[0] = in[1:], '-'
+	}
+
+	for i, j, k := len(in)-1, len(out)-1, 0; ; i, j = i-1, j-1 {
+		out[j] = in[i]
+		if i == 0 {
+			return string(out)
+		}
+		if k++; k == 3 {
+			j, k = j-1, 0
+			out[j] = ','
+		}
+	}
 }
--- qbit/queue-manager.go
+++ qbit/queue-manager.go
@@ -93,53 +93,90 @@ func (bqm *BasicQueueManager) Stop() error {
 	return err
 }
 
-func manageQueue(limit int, queueManager QueueManager, inputQueue ReceiveQueue, listener ReceiveQueueListener) {
+func manageQueue(batchSize int, queueManager QueueManager, inputQueue ReceiveQueue,
+	listener ReceiveQueueListener) {
 	listener.Init()
-	var item interface{}
+	var items []interface{}
 	count := 0
-	item = inputQueue.Poll() //Initialize things.
+
+	items = inputQueue.ReadBatch()
 
 OuterLoop:
 	for {
-		if item != nil {
-			listener.StartBatch()
-		}
 
-		for {
-			if item == nil {
-				break
+		if items != nil {
+			listener.StartBatch()
+			for i := 0; i < len(items); i++ {
+				count++
+				listener.Receive(items[i])
 			}
-			listener.Receive(item)
-			/* If the receive count has hit the max then we need to call limit. */
-			if count >= limit {
-				listener.EndBatch()
-				count = 0
+			listener.EndBatch()
+			if batchSize == len(items) {
+				recycleBuffer(batchSize, items)
+			}
+			items = inputQueue.ReadBatch()
+			continue OuterLoop
+		} else {
+			items = inputQueue.ReadBatchWait()
+			if items == nil {
+				listener.Empty()
 				if queueManager.Stopped() {
 					listener.Shutdown()
 					break OuterLoop
 				}
+
 			}
-			/* Grab the next item from the queue. */
-			item = inputQueue.Poll()
-			count++
-		}
-
-		count = 0
-		listener.Empty()
-
-		// Get the next item, but wait this time since the queue was empty.
-		// This pauses the queue handling so we don't eat up all of the CPU.
-		item = inputQueue.PollWait()
-		if queueManager.Stopped() {
-			listener.Shutdown()
-			break OuterLoop
-		}
-
-		if item == nil {
-			/* Idle means we yielded and then waited a full wait time, so idle might be a good time to do clean up
-			or timed tasks.
-			*/
-			listener.Idle()
 		}
 	}
 }
+
+//func manageQueue(limit int, queueManager QueueManager, inputQueue ReceiveQueue, listener ReceiveQueueListener) {
+//	listener.Init()
+//	var item interface{}
+//	count := 0
+//	item = inputQueue.Poll() //Initialize things.
+//
+//OuterLoop:
+//	for {
+//		if item != nil {
+//			listener.StartBatch()
+//		}
+//
+//		for {
+//			if item == nil {
+//				break
+//			}
+//			listener.Receive(item)
+//			/* If the receive count has hit the max then we need to call limit. */
+//			if count >= limit {
+//				listener.EndBatch()
+//				count = 0
+//				if queueManager.Stopped() {
+//					listener.Shutdown()
+//					break OuterLoop
+//				}
+//			}
+//			/* Grab the next item from the queue. */
+//			item = inputQueue.Poll()
+//			count++
+//		}
+//
+//		count = 0
+//		listener.Empty()
+//
+//		// Get the next item, but wait this time since the queue was empty.
+//		// This pauses the queue handling so we don't eat up all of the CPU.
+//		item = inputQueue.PollWait()
+//		if queueManager.Stopped() {
+//			listener.Shutdown()
+//			break OuterLoop
+//		}
+//
+//		if item == nil {
+//			/* Idle means we yielded and then waited a full wait time, so idle might be a good time to do clean up
+//			or timed tasks.
+//			*/
+//			listener.Idle()
+//		}
+//	}
+//}
